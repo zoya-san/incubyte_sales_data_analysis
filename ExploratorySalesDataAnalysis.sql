b10435b51a038253df6ae7a44bfb81a5b9a36580
zoya-san <Zoyashaikh1699@gmail.com>
2025-02-13 12:26:20 +0530

    Create ExploratorySalesDataAnalysis.sql
    Create ExploratorySalesDataAnalysis.sql

---
+++ ExploratorySalesDataAnalysis.sql
@@ -0,0 +1,4 @@
+select count(*) from incubyte.assessment_dataset
+-- # count(*)
+-- '450000'
+-- This count check is a data load validation, The total count of row matches the given assesment_dataset.csv
